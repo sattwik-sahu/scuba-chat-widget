bd0c294c48e21e6464a989c5310ce0f452e3a2e7
Sattwik Sahu <sahusattwik@gmail.com>
2024-04-09 15:24:17 +0530

    add:allowed ip addresses

--- vite.config.ts
+++ vite.config.ts
@@ -4,22 +4,32 @@ import envCompatible from "vite-plugin-env-compatible";
 import { nodePolyfills } from "vite-plugin-node-polyfills";
 
 export default defineConfig({
-  plugins: [react(), envCompatible(), nodePolyfills()],
-  build: {
-    lib: {
-      entry: "./src/components/index.tsx",
-      name: "ScubaChatWidget",
-      fileName: "index",
-      formats: ["umd", "iife"],
-    },
-    rollupOptions: {
-      external: ["react", "react-dom"],
-      output: {
-        globals: {
-          react: "React",
-          "react-dom": "ReactDOM",
-        },
-      },
+  plugins: [
+    react(),
+    // envCompatible(),
+    // nodePolyfills()
+  ],
+  // build: {
+  //   lib: {
+  //     entry: "./src/components/index.tsx",
+  //     name: "ScubaChatWidget",
+  //     fileName: "index",
+  //     formats: ["umd", "iife"],
+  //   },
+  //   rollupOptions: {
+  //     external: ["react", "react-dom"],
+  //     output: {
+  //       globals: {
+  //         react: "React",
+  //         "react-dom": "ReactDOM",
+  //       },
+  //     },
+  //   },
+  // },
+
+  server: {
+    cors: {
+      origin: [""], // TODO Put allowed origins to test application
     },
   },
 });
