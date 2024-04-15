3ac61961f3224daab6aecf1e71263e4c1b889efe
Sattwik Sahu <sahusattwik@gmail.com>
2024-04-15 19:20:21 +0530

    chores:remove bg img and placeholder text

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import ScubaChatWidget from "./components";
 const App: React.FC = () => {
   return (
     <div className="app">
-      This is the <code>App</code> component.
       <ScubaChatWidget apiKey="my-api-key" botName="Scuba Bot" />
     </div>
   );
