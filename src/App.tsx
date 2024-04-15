5a65078b442ca421e6abd9b93189e63bcd197fea
Sattwik Sahu <sahusattwik@gmail.com>
2024-04-15 19:21:51 +0530

    chores:change name of bot to "Ask IISERB"

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import ScubaChatWidget from "./components";
 const App: React.FC = () => {
   return (
     <div className="app">
-      <ScubaChatWidget apiKey="my-api-key" botName="Scuba Bot" />
+      <ScubaChatWidget apiKey="my-api-key" botName="Ask IISERB" />
     </div>
   );
 };
